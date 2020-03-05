07e2c4ddf2bbf326caca150b0ecae7ade5c32c5b
Ammyy9908 <sb78639@gmail.com>
2020-03-05 21:21:36 +0530

    Updates

---
+++ precache-manifest.bd6bc7f973f4d42b13000adbba419a18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868eb6d220864ee6c77bcb9e00d8ba29",
+    "url": "/Aditya/index.html"
+  },
+  {
+    "revision": "45f2440c483321a78be5",
+    "url": "/Aditya/static/js/2.9705ee16.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Aditya/static/js/2.9705ee16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34272e1b4e803a044635",
+    "url": "/Aditya/static/js/main.fb016d43.chunk.js"
+  },
+  {
+    "revision": "71eeeedb6469326e137d",
+    "url": "/Aditya/static/js/runtime-main.b77d0614.js"
+  }
+]);
